c6ad4848142e2a73dc7028f73f675b6cd7982032
USUARIO <ana-neves98@outlook.com>
2023-08-22 16:24:32 -0300

    Add project files.

---
+++ Dolar/ConversorDeMoedas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolar {
+    class ConversorDeMoedas {
+        public static double Valor(double x, double y) {
+
+            return x * y; //620
+        }
+        public static double ValorTotal(double resultado) {
+
+            double porcentagem = 6.0 / 100.0; //6% decimal
+            double valorDaPorcentagem = resultado * porcentagem;
+            double total = valorDaPorcentagem + resultado; // Adicionando a porcentagem ao valor original.
+           
+            return total;
+        }
+    }
+}
---
+++ Dolar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Dolar;
+using System.Globalization;
+
+namespace Dolar {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Qual é a cotação do dolar? ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantos dólares você vai comprar? ");
+            double y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            double resultado = ConversorDeMoedas.Valor(x, y);
+            double porcentagem = ConversorDeMoedas.ValorTotal(resultado);
+
+            Console.WriteLine("\nValor a ser pago em reais: " + porcentagem.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
